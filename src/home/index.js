788bd51db5f4c80082c277cf8b51d1f567abff24
JUNJIE WANG <>
2022-03-14 16:48:43 +0800

    update

--- src/home/index.js
+++ src/home/index.js
@@ -1,16 +1,26 @@
 import { Input } from 'antd';
-import './index.css';
+import { useState } from 'react';
+import './index.less';
+
+const { Search } = Input;
 
 const App = () => {
   const height = window.innerHeight;
-
+  const [currentLocal, setCurrentLocal] = useState('');
   return (
     <section className="App" style={{ height: height }}>
       <div className='homePage-title'>
         PARKING JUST GET A LOT FASTER
       </div>
-      <div>
-        <Input />
+      <div className='homepage-search'>
+        <Search
+          allowClear
+          placeholder='Search Place, Postal Codes or District'
+          enterButton="Search"
+          value={currentLocal}
+          size='large'
+          onChange={setCurrentLocal}
+        />
       </div>
       <div>
 
