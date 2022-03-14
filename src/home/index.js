87e84846e1df3f513845daf3d1e4f499673904ae
JUNJIE WANG <>
2022-03-14 14:55:32 +0800

    create project

---
+++ src/home/index.js
@@ -0,0 +1,13 @@
+import './index.css';
+
+const App = () => {
+  const height = window.innerHeight;
+
+  return (
+    <section className="App" style={{ height: height }}>
+
+    </section>
+  );
+}
+
+export default App;
