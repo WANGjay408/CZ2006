5196e1c0b144803bf67f06207614db81f91bda83
JUNJIE WANG <>
2022-03-28 13:38:47 +0800

    update

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -1,11 +1,21 @@
+import { useState } from 'react';
 import './index.css';
 
 const App = () => {
   const height = window.innerHeight;
+  const [endLocation, setEndLoaction] = useState('');
   
   return (
     <section className="App" style={{ height: height }}>
-      
+      <div className='title'>
+        PARKING JUST GET A LOT FASTER
+      </div>
+      <div className='search-input'>
+        <input type='text' value={endLocation}/>
+        <button onClick={() => {}}>
+          
+        </button>
+      </div>
     </section>
   );
 }
